fbcaf60ffaa42e57fbf41ebb2274792834e847bd
hugopgs <127613503+hugopgs@users.noreply.github.com>
2023-12-08 16:07:53 +0100

    Update Recalage.h

--- TP/include/Recalage.h
+++ TP/include/Recalage.h
@@ -21,9 +21,10 @@ public:
 	void construireFichier(const string&, const NRmatrix<double>) const; // ecrire une image depuis une matrice dans le fichier dont le nom est passé en entrée 
 	void construireFichier(const string&, const NRmatrix<bool>) const; // ecrire une image depuis une matrice dans le fichier dont le nom est passé en entrée 
 	
-	VecDoub getThetaMax(string f,
-		string fRef,
-		string fwrite,
-		Similarite* sim = 0, 
-		Interpolation* interp = 0); // renvoie le vecteur de transformation qui minimise le cout
+VecDoub getThetaMax(string f, string fRef, string fwrite, Similarite* sim = 0, Interpolation* interp = 0); // renvoie le vecteur de transformation qui minimise le cout
+VecDoub getThetaMax(string f, string fRef, Similarite* sim = 0, Interpolation* interp = 0);
+VecDoub getThetaMax(string f, string fRef, Interpolation* interp);
+VecDoub getThetaMax(string f, string fRef, Similarite* sim);
+VecDoub getThetaMax(string f, string fRef, string fwrite, Interpolation* interp);
+VecDoub getThetaMax(string f, string fRef, string fwrite, Similarite* sim);
 };
