b6aba8532e735dae0fb02ce454ea7fdaca42d174
emmadfr <150262598+emmadfr@users.noreply.github.com>
2023-12-08 16:00:50 +0100

    Add files via upload

---
+++ TP/src/Transformation.cpp
@@ -0,0 +1,13 @@
+#include "Transformation.h"
+#include <math.h>
+
+void Transformation ::getNewpixels(
+    double x, double y,    // coord avant transformation
+    const double *THETA,   // THETA=[tx,ty,theta]
+    double &xp, double &yp // coord après transfo
+    ) // renvoie les coordonnees du pixel apres transformation
+{
+  double theta = THETA[2];
+  xp = cos(theta) * x - sin(theta) * y + THETA[0];
+  yp = sin(theta) * x + cos(theta) * y + THETA[1];
+}
